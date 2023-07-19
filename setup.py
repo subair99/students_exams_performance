010e09982e847c72ba0d116b98535aa43ce00705
subair99 <subair9@gmail.com>
2023-07-19 16:19:08 +0100

    Model Trainer

--- setup.py
+++ setup.py
@@ -17,11 +17,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='students_exams_performance',
-version='0.0.1',
-author='AbdulKabir',
-author_email='subair9@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
-
-)
+        name='students_exams_performance',
+        version='0.0.1',
+        author='AbdulKabir',
+        author_email='subair9@gmail.com',
+        packages=find_packages(),
+        install_requires=get_requirements('requirements.txt')
+    )
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -94,13 +94,12 @@ class ModelTrainer:
             best_model_score = max(sorted(model_report.values()))
 
             ## To get best model name from dict
-
             best_model_name = list(model_report.keys())[
                                                     list(model_report.values()).index(best_model_score)
                                                        ]
             best_model = models[best_model_name]
 
-            if best_model_score<0.6:
+            if best_model_score < 0.6:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
 
@@ -115,9 +114,6 @@ class ModelTrainer:
 
             return r2_square
             
-
-
-
             
         except Exception as e:
             raise CustomException(e, sys)
